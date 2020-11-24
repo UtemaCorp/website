acec213377e24a7b4b3feb697c9f1b2b672af4a9
Ronteah <light.louis@hotmail.fr>
2020-11-24 15:19:05 +0100

    Portfolio Louis (oui encore)

--- portfolio/louis/assets/js/top.js
+++ portfolio/louis/assets/js/top.js
@@ -1,12 +1,12 @@
 var nav = document.getElementById('nav');
 
 window.onscroll = function(){
-    if (window.pageYOffset >100) {
-        nav.style.background = "#f55656";
-        nav.style.color = "#fff";
-    }
-    else {
+    if (window.pageYOffset <300) {
         nav.style.background = "transparent";
         nav.style.color = "transparent";
+    }
+    else {
+        nav.style.background = "#f55656";
+        nav.style.color = "#fff";
     }
 }
